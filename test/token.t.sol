8d5a13e230526c60dfa8a13da837e07f710bebd6
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-06-16 23:58:19 +0530

    Aprrove and transfer test

--- test/token.t.sol
+++ test/token.t.sol
@@ -12,8 +12,29 @@ contract TokenTest is Test {
     }
 
     function testMint() public {
-        mytoken.mint(address(this), 100);
-        uint256 expected = 100;
-        assertEq(mytoken.balanceOf(address(this)), expected, "OKK");
+        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        address owner = address(this);
+        mytoken.mint(owner, 100);
+        mytoken.transfer(user, 50);
+        uint256 expected = 50;
+        assertEq(mytoken.balanceOf(owner), expected, "OKK");
+        // this is here the person who deployed the contract
+        assertEq(
+            mytoken.balanceOf(user),
+            50,
+            "OKK2"
+        );
+        vm.prank(user);
+        mytoken.transfer(owner, 50);
+
+        mytoken.approve(user, 10);
+        vm.prank(user);
+        mytoken.transferFrom(
+            owner,
+            user, 
+            10
+        );
+        // makes user to call the function as if they are the owner
+        assertEq(mytoken.balanceOf(user), 10, "OKK3");
     }
 }
