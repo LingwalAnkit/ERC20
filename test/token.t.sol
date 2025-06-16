8ca7fc9015ed19e1a9c5237eb0a48d8def882a02
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-06-17 00:05:17 +0530

    Changes

--- test/token.t.sol
+++ test/token.t.sol
@@ -19,22 +19,35 @@ contract TokenTest is Test {
         uint256 expected = 50;
         assertEq(mytoken.balanceOf(owner), expected, "OKK");
         // this is here the person who deployed the contract
-        assertEq(
-            mytoken.balanceOf(user),
-            50,
-            "OKK2"
-        );
+        assertEq(mytoken.balanceOf(user), 50, "OKK2");
         vm.prank(user);
         mytoken.transfer(owner, 50);
 
         mytoken.approve(user, 10);
         vm.prank(user);
-        mytoken.transferFrom(
-            owner,
-            user, 
-            10
-        );
+        mytoken.transferFrom(owner, user, 10);
         // makes user to call the function as if they are the owner
         assertEq(mytoken.balanceOf(user), 10, "OKK3");
+    }
+
+    function testTransfer() public {
+        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        address owner = address(this);
+        mytoken.mint(owner, 100);
+        mytoken.transfer(user, 50);
+        assertEq(mytoken.balanceOf(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2), 50, "OKK2");
+    }
+
+    function testApprove() public {
+        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+        address owner = address(this);
+        mytoken.mint(owner, 100);
+        mytoken.transfer(user, 50);
+
+        mytoken.approve(user, 10);
+        vm.prank(user);
+        mytoken.transferFrom(owner, user, 10);
+        // makes user to call the function as if they are the owner
+        assertEq(mytoken.balanceOf(user), 60, "OKK3");
     }
 }
