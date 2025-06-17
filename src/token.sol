065f9679ab90da19aec7775fac2cac7e9b70becd
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-06-17 12:03:53 +0530

    negative tests and std libs

--- src/token.sol
+++ src/token.sol
@@ -1,20 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20}  from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor() ERC20("AnkitCoin", "ANK") Ownable(msg.sender) {
-    }
-    // here msg.sender is the one who deploys the contract
 
+contract MyToken is ERC20, Ownable {
+
+    constructor() ERC20("AnkitCoin", "ANK") Ownable(msg.sender) {}
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
     function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+        _burn(_msgSender(), amount);
     }
-    // this func allow any user to burn their token
 }
--- test/token.t.sol
+++ test/token.t.sol
@@ -6,48 +6,70 @@ import {MyToken} from "../src/token.sol";
 
 contract TokenTest is Test {
     MyToken public mytoken;
+    address owner;
+    address user;
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     function setUp() public {
         mytoken = new MyToken();
+        owner = address(this);
+        user = address(0xABCD);
     }
 
     function testMint() public {
-        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        address owner = address(this);
         mytoken.mint(owner, 100);
-        mytoken.transfer(user, 50);
-        uint256 expected = 50;
-        assertEq(mytoken.balanceOf(owner), expected, "OKK");
-        // this is here the person who deployed the contract
-        assertEq(mytoken.balanceOf(user), 50, "OKK2");
-        vm.prank(user);
-        mytoken.transfer(owner, 50);
-
-        mytoken.approve(user, 10);
-        vm.prank(user);
-        mytoken.transferFrom(owner, user, 10);
-        // makes user to call the function as if they are the owner
-        assertEq(mytoken.balanceOf(user), 10, "OKK3");
+        assertEq(mytoken.balanceOf(owner), 100, "Owner should have 100 tokens");
     }
 
     function testTransfer() public {
-        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        address owner = address(this);
         mytoken.mint(owner, 100);
-        mytoken.transfer(user, 50);
-        assertEq(mytoken.balanceOf(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2), 50, "OKK2");
+        mytoken.transfer(user, 40);
+        assertEq(mytoken.balanceOf(user), 40, "User should have 40 tokens");
+        assertEq(mytoken.balanceOf(owner), 60, "Owner should have 60 tokens");
     }
 
-    function testApprove() public {
-        address user = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        address owner = address(this);
+    function testApproveAndTransferFrom() public {
         mytoken.mint(owner, 100);
-        mytoken.transfer(user, 50);
+        mytoken.approve(user, 50);
 
-        mytoken.approve(user, 10);
         vm.prank(user);
-        mytoken.transferFrom(owner, user, 10);
-        // makes user to call the function as if they are the owner
-        assertEq(mytoken.balanceOf(user), 60, "OKK3");
+        mytoken.transferFrom(owner, user, 30);
+
+        assertEq(mytoken.balanceOf(user), 30, "User should have 30 tokens");
+        assertEq(
+            mytoken.allowance(owner, user),
+            20,
+            "Remaining allowance should be 20"
+        );
+        assertEq(mytoken.balanceOf(owner), 70, "Owner should have 70 tokens");
+    }
+
+    function test_approvalOverLimitTransfer_reverts() public {
+        mytoken.mint(owner, 100);
+        mytoken.approve(user, 20);
+
+        vm.prank(user);
+        vm.expectRevert(); // or pass the selector / reason string
+        mytoken.transferFrom(owner, user, 100);
+    }
+
+    function test_transfer_reverts() public {
+        mytoken.mint(owner, 100);
+        vm.expectRevert();
+        mytoken.transfer(user, 200);
+    }
+
+    function testTranferEmits() public {
+        mytoken.mint(owner , 100);
+        vm.expectEmit(true, true, false, true); // match from to and value the false means we don't care about the 3rd indexed field
+        emit Transfer(owner, user, 50);
+        mytoken.transfer(user, 50);
+    }
+
+    function testDeal() public {
+        address user2 = address(0x1234);
+        vm.deal(user2, 10 ether);
+
+        assertEq(user2.balance, 10 ether);
     }
 }
